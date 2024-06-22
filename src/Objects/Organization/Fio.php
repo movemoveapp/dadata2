08fd68318ea230c85ff7d3ebebf309ee95c52178
Dmitry Kovalev <sub@pipisco.com>
2024-06-22 18:31:52 +0300

    feature/organization-find-by-id: added description of the customer (ФИО) structure: first/middle/last names

---
+++ src/Objects/Organization/Fio.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace MoveMoveApp\DaData2\Objects\Organization;
+
+use MoveMoveApp\DaData2\Objects\BaseObject;
+
+/**
+ * @property string|null $surname
+ * @property string|null $name
+ * @property string|null $patronymic
+ * @property string|null $gender
+ * @property string|null $source
+ * @property string|null $qc
+ */
+class Fio extends BaseObject
+{
+    protected array $attributes = [
+        'surname'       => 'string|null',
+        'name'          => 'string|null',
+        'patronymic'    => 'string|null',
+        'gender'        => 'string|null',
+        'source'        => 'string|null',
+        'qc'            => 'string|null',
+    ];
+}
