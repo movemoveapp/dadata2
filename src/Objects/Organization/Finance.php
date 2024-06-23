9be418775a7c2c1295fbaa455036f8285be68fae
Dmitry Kovalev <sub@pipisco.com>
2024-06-23 11:16:13 +0300

    feature/organization: descrtibe finance object parameters

--- src/Objects/Organization/Finance.php
+++ src/Objects/Organization/Finance.php
@@ -6,6 +6,12 @@ use MoveMoveApp\DaData2\Objects\BaseObject;
 
 /**
  * @property string|null $tax_system
+ * @property string|null $year
+ * @property string|null $income
+ * @property string|null $revenue
+ * @property string|null $expense
+ * @property string|null $debt
+ * @property string|null $penalty
  */
 class Finance extends BaseObject
 {
